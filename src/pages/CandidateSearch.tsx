439de86708078901e5cef18a76bc68eea366bf04
morgan4491 <m.morgan4491@gmail.com>
2024-10-31 16:38:05 -0400

    saving candidate search function

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,31 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import { Candidate } from '../interfaces/Candidate.interface';
+
+
 
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+
+  const [ candidateState, setCandidateState ] = useState<Candidate[]>([]);
+
+  useEffect(() => {
+    async function getCandidates() {
+      const candidateArray : Candidate[] = await searchGithub()
+      
+      setCandidateState(candidateArray);
+    }
+    
+    getCandidates();
+  }, [])
+
+  return (
+    <div>
+
+      <h1>CandidateSearch</h1>
+      {candidateState.length>0 && <p>{candidateState[0].login}</p>}
+    </div>
+    
+  )
 };
 
 export default CandidateSearch;
