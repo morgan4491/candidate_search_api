09b1410a49117686978d9eb546d9151bdb9e2081
morgan4491 <m.morgan4491@gmail.com>
2024-10-31 16:38:05 -0400

    saving candidate search function

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,9 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+
+export interface Candidate {
+    login: string;
+    location?: string | null;
+    email: string;
+    company: string;
+    bio: string
+}
