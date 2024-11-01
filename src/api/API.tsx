5d63c4ba594bd8cb1064984f44024b3348378609
morgan4491 <m.morgan4491@gmail.com>
2024-11-01 11:38:32 -0400

    need to finish with styling

--- src/api/API.tsx
+++ src/api/API.tsx
@@ -10,7 +10,7 @@ const searchGithub = async () => {
         },
       }
     );
-    // console.log('Response:', response);
+    console.log('Response:', response);
     const data = await response.json();
     if (!response.ok) {
       throw new Error('invalid API response, check the network tab');
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
     <>
-      <a>Home</a>
-      <a>Potential Candidates</a>
+      <nav><Link to="/">Home</Link></nav>
+      <nav><Link to="/SavedCandidates">Potential Candidates</Link></nav>
     </> 
   )
 };
