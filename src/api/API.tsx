c7896e441c52cade363baaea95bee1ca0eafd04d
morgan4491 <m.morgan4491@gmail.com>
2024-11-03 11:51:00 -0500

    saving new token

--- src/api/API.tsx
+++ src/api/API.tsx
@@ -6,7 +6,7 @@ const searchGithub = async () => {
       `https://api.github.com/users?since=${start}`,
       {
         headers: {
-          Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+          Authorization: `Bearer ${import.meta.env.GITHUB_TOKEN}`,
         },
       }
     );
@@ -28,7 +28,7 @@ const searchGithubUser = async (username: string) => {
   try {
     const response = await fetch(`https://api.github.com/users/${username}`, {
       headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+        Authorization: `Bearer ${import.meta.env.GITHUB_TOKEN}`,
       },
     });
     const data = await response.json();
